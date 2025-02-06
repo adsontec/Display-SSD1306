64d7590c04012d916ebce7a7d4115da6027a7ec7
Adson <adson.oliveira.inf@gmail.com>
2025-02-06 17:56:27 -0300

    Botão A e Botão B - Feito

--- DisplayC.c
+++ DisplayC.c
@@ -6,10 +6,6 @@
 #include "inc/font.h"
 
 // Define o Pinos GPIO
-#define UART_ID uart0 // Seleciona a UART0
-#define BAUD_RATE 115200 // Define a taxa de transmissão
-#define UART_TX_PIN 0 // Pino GPIO usado para TX
-#define UART_RX_PIN 1 // Pino GPIO usado para RX
 #define GPIO_R_LED 13
 #define GPIO_B_LED 12
 #define GPIO_G_LED 11
@@ -34,36 +30,36 @@ void init_gpio();
 void init_display();
 void ssd1306();
 void gpio_irq_handler(uint gpio, uint32_t events);
-void uart_gpio();
 
 int main(){
+  stdio_init_all();
   inicializar_GPIOs();
   i2c_init(I2C_PORT, 400 * 1000);
   init_gpio();
   init_display();
-  uart_gpio();
-
-  // Mensagem inicial
-  const char *init_message = "UART Demo - RP2\r\n"
-                               "Digite algo e veja o eco:\r\n";
-  uart_puts(UART_ID, init_message);
 
   // Configuração da interrupção com callback
   gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
-
+  gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+  printf("RP2040 inicializado\n");
+  
   while (true){
-    if (uart_is_readable(UART_ID)) {
+    if(stdio_usb_connected()) {
       // Lê um caractere da UART
-      char c = uart_getc(UART_ID), letra = c;
-      char texto[2] = {letra, '\0'}; // Criamos uma string com um único caractere
-
-      ssd1306_fill(&ssd, !cor); // Limpa o display
-      ssd1306_rect(&ssd, 3, 3, 122, 58, cor, !cor); // Desenha um retângulo
-      ssd1306_draw_string(&ssd, "INSIRA O DADO" , 8, 10); // Desenha uma string
-      ssd1306_draw_string(&ssd, texto, 20, 30); // Desenha uma string     
-      ssd1306_send_data(&ssd);
+      char c, letra = c;
+        if ((c = getchar()) != EOF) { // Lê um caractere da entrada padrão
+            char texto[2] = {c, '\0'}; // Converte o caractere em string
+            
+            ssd1306_fill(&ssd, !cor); // Limpa o display antes de desenhar
+            ssd1306_rect(&ssd, 3, 3, 122, 58, cor, !cor); // Desenha um retângulo na tela
+            ssd1306_draw_string(&ssd, "INSIRA O DADO", 8, 10); // Exibe mensagens no display
+            ssd1306_draw_string(&ssd, texto, 20, 30); // Exibe o caractere digitado
+            ssd1306_send_data(&ssd); // Atualiza o display com os novos dados
+        }
     }
+    sleep_ms(40);
   }
+  return 0;
 }
 
 void init_gpio(){
@@ -99,7 +95,7 @@ void inicializar_GPIOs(){
 
 // Função de interrupção com debouncing
 void gpio_irq_handler(uint gpio, uint32_t events){
-  bool btn_last_state = 0;
+    bool btn_last_state = 0;
 
     // Obtém o tempo atual em microssegundos
     uint32_t current_time = to_us_since_boot(get_absolute_time());
@@ -110,16 +106,19 @@ void gpio_irq_handler(uint gpio, uint32_t events){
         last_press_time = get_absolute_time();
         btn_last_state = 1;
         if (gpio == BUTTON_A){
+          printf("LED verde %s\n", (gpio_get(GPIO_G_LED) == 0) ? "ligado" : "desligado");
+          ssd1306_fill(&ssd, !cor); // Limpa o display
+          ssd1306_draw_string(&ssd,(gpio_get(GPIO_G_LED) == 0) ? "G LIGADO" : "G DESLIGADO", 15, 48);
+          ssd1306_send_data(&ssd);
           gpio_put(GPIO_G_LED, !gpio_get(GPIO_G_LED));
+        }else if ((gpio == BUTTON_B)){
+          printf("LED Azul %s\n", (gpio_get(GPIO_B_LED) == 0) ? "ligado" : "desligado");
+          ssd1306_fill(&ssd, !cor); // Limpa o display
+          ssd1306_draw_string(&ssd,(gpio_get(GPIO_B_LED) == 0) ? "B LIGADO" : "B DESLIGADO", 15, 48);
+          ssd1306_send_data(&ssd);
+          gpio_put(GPIO_B_LED, !gpio_get(GPIO_B_LED));
         }
     }else if (!btn_pressed){
         btn_last_state = 0;
     }
-}
-
-void uart_gpio(){
-  uart_init(UART_ID, BAUD_RATE);
-  // Configura os pinos GPIO para a UART
-  gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART); // Configura o pino 0 para TX
-  gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART); // Configura o pino 1 para RX
 }
